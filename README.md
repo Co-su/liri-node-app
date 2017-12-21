3d790759850d91d2d15122aea6cac3f8b3c6add0
Co-Su <thisusernameisnottaken@gmail.com>
2017-12-20 17:05:42 -0800

    lots of stuff doesn't work, but I've run out of time and need to consult a TA.
    q

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # liri-node-app
+
+Some notable issues:
+
+I'm not sure how to assign each individual function with a command line prompt, so it's hampering my progress on troubleshooting certain aspects of my code.
+
+-update-
+
+I might have the command working?  maybe?
+
+-update 2-
+
+Nevermind, still lost.  Submitting as-is and seeking TA assistance following day.
--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 //*DISCLAIMER* I don't know how to assign individual command line calls to these individual modules.  
 //Hoping to get some time with a TA to clarify.
 
+
 //OMDB Request
 var request = require("request");
 var movieName = process.argv[2];
@@ -15,14 +16,42 @@ request(queryUrl, function(error, response, body) {
     console.log("Language: " + JSON.parse(body).Language);
     console.log("Plot: " + JSON.parse(body).Plot);
     console.log("Cast: " + JSON.parse(body).Actors);
-  }
-});
-//Twitter Request
-var request = require("request");
-var myTweets = process.argv[3];
-var queryUrl = "https://api.twitter.com/1.1/statuses/retweets/509457288717819904.json";
-request(queryUrl, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
+  	
+  	} else if (command == "spotify-this-song"){
+    
+    //Spotify Song Search
+    songName = process.argv[3];
+    var Spotify = require("node-spotify-api");
+    var spotify = new Spotify({
+      id: "01f6dfc0bbd04591bbe5ffda7cc4c559",
+      secret: "72aa7eae9afd45388ab474a9d10cb808",
+    });
+    spotify.search({
+    	type: "track",
+    	query: songName
+    		}, function(err, data) {
+      			if (err) {
+        	return console.log("Error: " + err);
+      	}
+    	console.log("Artist(s): " +data.tracks.items[0].album.artists[0].name);
+    	console.log("The song's name: " +data.tracks.items[0].name);
+    	console.log("Song preview: " +data.tracks.items[0].preview_url);
+    	console.log("Album: " +data.tracks.items[0].album.name); 
+		});
+	
+	}else if (command == "tweetMe"){
+	
+	//Twitter Search
+	var myTweets = process.argv[4];
+	var queryUrl = "https://api.twitter.com/1.1/statuses/tweets/509457288717819904.json";
+	request(queryUrl, function(error, response, body) {
+  	if (!error && response.statusCode === 200) {
     console.log(JSON.parse(body).text);
-  }
+  			}
+		}
+	)};
 });
+
+//Spotify Request
+
+
